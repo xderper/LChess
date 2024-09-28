c571ed8912ff8ce58329744e5c62fe4c084b2683
Artem <temaosi2008@gmail.com>
2024-09-28 13:40:51 +0300

    ip

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.use(express.static('.'));
-app.use(express.static('./admin/src'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+app.get('*', (req, res) => {
+    res.status(403).send('Access denied');
+});
 
 
 app.listen(3000);
--- src/login.js
+++ src/login.js
@@ -2,5 +2,7 @@ const send_btn = document.getElementById('send_btn');
 const code_input = document.getElementById('code_input');
 
 send_btn.addEventListener('click', () => {
-    console.log(code_input.value)
+    if (code_input.value == 'd1a2'){
+        window.location.href = "../public/game.html";
+    }
 })
